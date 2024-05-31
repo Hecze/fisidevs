b283f71a2ac2a291698d3079f6648edbe91dc68b
Hecze <hectorzerrillo85@gmail.com>
2024-05-31 14:11:17 -0500

    responsive 2000%

--- src/app/components/CardContainer.jsx
+++ src/app/components/CardContainer.jsx
@@ -3,7 +3,11 @@ import Card from "./Card";
 
 const CardContainer = ({ children }) => {
   return (
-    <div className="grid gap-3 grid-flow-dense auto-rows-[11rem] w-full h-full " style={{ gridTemplateColumns: 'repeat(auto-fill, minmax(22rem, 1fr))' }}>
+    <div className="grid gap-3 grid-flow-dense auto-rows-[11rem] w-full h-full " style={{ gridTemplateColumns: 'repeat(auto-fill, minmax(23rem, 1fr))' }}>
+      <div className="bg-blue-700 col-span-1 row-span-2 hidden"/>
+      <div className="bg-red-700  col-span-1 row-span-2  md:col-span-2 md:row-span-1 hidden"/>
+      <div className="bg-yellow-700 col-span-1 row-span-1 hidden"/>
+      <div className="bg-red-200 col-span-1 row-span-1 hidden"/>
       {children}
     </div>
   );
