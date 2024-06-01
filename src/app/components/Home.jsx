dcea7b2fce21237fec1729f2a463ecb913079556
Hecze <hectorzerrillo85@gmail.com>
2024-05-31 20:53:44 -0500

    menos gapping entre secciones en android

--- src/app/components/Home.jsx
+++ src/app/components/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Home = () => {
   return (
-    <div className="h-52 min-[320px]:h-[16rem] min-[550px]:h-[22rem] sm:h-[25rem] md:h-[36rem] lg:h-[48rem] 2xl:h-screen w-full flex justify-center">
+    <div className="h-[14.5rem] min-[440px]:h-[22rem] sm:h-[25rem] md:h-[36rem] lg:h-[48rem] 2xl:h-screen w-full flex justify-center">
       <img
         className=" w-4/5 max-w-[90vw] z-20 absolute  lg:top-[-6rem]"
         alt="image"
@@ -14,7 +14,7 @@ const Home = () => {
         src="/diffuser2.svg"
       />
       <img
-        className=" w-full h-[20rem] md:h-[30rem]  2xl:h-[56rem]  absolute top-0 left-0 z-10"
+        className=" w-full h-[18rem] md:h-[30rem]  2xl:h-[56rem]  absolute top-0 left-0 z-10"
         alt="image"
         src="/items.svg"
       />
